79ec1197ea88a91e585d2f256fbe7d1a0d09ca89
Joanna Hughes <jhughes@vitamix.com>
2018-12-12 09:35:29 -0500

    adding all files

---
+++ js/360.js
@@ -0,0 +1,20 @@
+var image360 = document.getElementById("image360");
+var slider = document.getElementById("slider");
+var imageNo = 1;
+var images = [];
+
+//Preloading images
+for (var i = 1; i < 25; i++){
+    images[i] = new Image();
+    images[i].src = "images/ascent-a2500-black_" + i + ".jpg";
+}
+
+//Event handler for input from the slider as the user slides it
+slider.addEventListener("input", function(){
+    image360.src = images[slider.value].src;
+});
+
+//Function to switch out to the next image
+function rotate(){
+    if(imageNo === 25) imageNo = 1;
+}
