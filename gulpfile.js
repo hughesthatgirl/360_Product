9ae07c7d93ad7aa90a9dc3398395dd2b32158f3f
Joanna Hughes <jhughes@vitamix.com>
2018-12-12 09:46:45 -0500

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// Gulp.js configuration
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var data = require('gulp-data');
+
+// ------------------------------------------
+// Static Server - Page Reload
+// ------------------------------------------
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+// ------------------------------------------
+// Watch Tasks
+// ------------------------------------------
+gulp.task('watch', ['browserSync'], function (){
+    // Reloads the browser whenever our HTML or CSS files change
+    gulp.watch('app/**/*.+(html|css)', browserSync.reload);
+
+});
