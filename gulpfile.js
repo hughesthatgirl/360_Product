a8692bf66fd10af9000f3b5f05207b3293b1259d
Joanna Hughes <jhughes@vitamix.com>
2018-12-13 11:29:02 -0500

    updating gulpfile so that its watching js files for changes

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,6 @@ gulp.task('browserSync', function() {
 // ------------------------------------------
 gulp.task('watch', ['browserSync'], function (){
     // Reloads the browser whenever our HTML or CSS files change
-    gulp.watch('app/**/*.+(html|css)', browserSync.reload);
+    gulp.watch('app/**/*.+(html|css|js)', browserSync.reload);
 
 });
