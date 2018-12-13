8ca8351701918d920d21e98d324da346c05f1946
Joanna Hughes <jhughes@vitamix.com>
2018-12-13 10:00:21 -0500

    using data attributes to set image path

--- app/js/360.js
+++ app/js/360.js
@@ -1,4 +1,7 @@
 var image360 = document.getElementById("image360");
+var productName = image360.getAttribute("data-product-name");
+var productNumber = image360.getAttribute("data-product-number");
+var productColor = image360.getAttribute("data-product-color");
 var slider = document.getElementById("slider");
 var imageNo = 1;
 var images = [];
@@ -6,7 +9,7 @@ var images = [];
 //Preloading images
 for (var i = 1; i < 25; i++){
     images[i] = new Image();
-    images[i].src = "/images/ascent-a2500-black_" + i + ".jpg";
+    images[i].src = "/images/" + productName + "-" + productNumber + "-" + productColor + "_" + i + ".jpg";
 }
 
 //Event handler for input from the slider as the user slides it
